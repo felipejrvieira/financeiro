8e6d038d409f414b9aef27c77cd52ea110d40f28
Felipe J. R. Vieira <felipejrvieira@gmail.com>
2021-03-13 17:42:05 -0300

    Estruturação dos diretórios, configuração do docker e organização das rotas.

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SegmentoController;
+use App\Http\Controllers\AcaoController;
+use App\Http\Controllers\OrdemController;
+use App\Http\Controllers\ProventoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +21,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::apiResources([
-    'segmentos' => SegmentoController::class,
-    'acoes' => AcaoController::class,
-    'ordens' => OrdemController::class,
-    'proventos' => ProventoController::class,
-]);
+Route::apiResource('segmentos',SegmentoController::class);
+Route::apiResource('proventos', ProventoController::class);
+Route::apiResource('acoes',AcaoController::class)->parameters(['acoes' => 'acao']);
+Route::apiResource('ordens',OrdemController::class)->parameters(['ordens' => 'ordem']);
