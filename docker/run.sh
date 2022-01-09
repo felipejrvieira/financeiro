e8a9ff0026e8a63e998daed937107020aa0940b8
Felipe J. R. Vieira <felipejrvieira@gmail.com>
2022-01-08 21:32:50 -0300

    Alterações na configurações para construir o ambiente na primeira execução

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-
+composer install
+php -r "file_exists('.env') || copy('.env.example', '.env');"
+php artisan key:generate --ansi
+touch database/database.sqlite
+php artisan migrate:fresh --seed
 php artisan serve --host=0.0.0.0 --port=80
